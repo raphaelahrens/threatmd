dce92ada63a076479a46266f01704badd6cec3e5
Raphael Ahrens <raphaelahrens@googlemail.com>
2024-04-30 15:01:18 +0200

    Inital commit of threatrs
    threatrs is a library for reading in threats in markdown and composing
    them to threats in the JSON format opf pytm

---
+++ src/bin/threats_pytm.rs
@@ -0,0 +1,113 @@
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::fs::read_to_string;
+use std::io;
+
+use pulldown_cmark::HeadingLevel;
+
+use clap::Parser;
+use eyre::{eyre, Result};
+
+use serde::{Serialize, Deserialize};
+
+use threatrs::{MarkdownIter, MarkdownParser};
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The directory storing the markdown threats
+    threat_dir: PathBuf,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct MetaField{
+    sid: String,
+    severity: String,
+    target: Vec<String>,
+    likelihood: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Threat{
+    #[serde(rename = "SID")]
+    sid: String,
+    severity: String,
+    target: Vec<String>,
+    description: String,
+    details: String,
+    example: String,
+    mitigations: String,
+    condition: String,
+    references: String,
+    prerequisites: String,
+    #[serde(rename = "Likelihood Of Attack")]
+    likelihood: String,
+}
+
+fn parse_md(markdown_input: &str) -> Result<Threat>{
+    let p = MarkdownParser::new(markdown_input);
+    let mut parser = p.iter();
+
+    let metadata = parser.metadata()?;
+
+    let metadata: MetaField = serde_yaml::from_str(&metadata)?;
+
+    let description = parser.heading(HeadingLevel::H1)?;
+
+
+    let details = p.to_string(parser.multi(MarkdownIter::text));
+
+    parser.named_heading(HeadingLevel::H2, "Example")?;
+    let example = p.to_string(parser.multi(MarkdownIter::text));
+
+    parser.named_heading(HeadingLevel::H2, "Mitigations")?;
+    let mitigations = p.to_string(parser.multi(MarkdownIter::text));
+
+    parser.named_heading(HeadingLevel::H2, "Condition")?;
+    let condition = p.get_text(parser.lang_block("python")?);
+
+    parser.named_heading(HeadingLevel::H2, "Prerequisites")?;
+    let prerequisites = p.to_string(parser.multi(MarkdownIter::text));
+    parser.named_heading(HeadingLevel::H2, "References")?;
+    let refernces = parser.item_list()?;
+    Ok(Threat{
+        sid: metadata.sid,
+        severity: metadata.severity,
+        target: metadata.target,
+        likelihood: metadata.likelihood,
+        description,
+        details,
+        example,
+        mitigations,
+        condition,
+        prerequisites,
+        references: refernces.join(", "),
+    })
+}
+
+fn main() -> Result<()>{
+    let cli = Cli::parse();
+
+    let mut threats = vec![];
+
+    let mut stdout = io::stdout().lock();
+
+    if !cli.threat_dir.is_dir() {
+        return Err(eyre!("The given path was not a directory"));
+    }
+
+    for entry in cli.threat_dir.read_dir().expect("expected to the directory to be readable."){
+        if let Ok(entry) = entry {
+            let entry = entry.path();
+            if Some(OsStr::new("md")) == entry.extension() {
+                let md_input = read_to_string(&entry)?;
+                threats.push(parse_md(&md_input)?);
+            }
+        }
+    }
+
+    serde_json::to_writer_pretty(&mut stdout, &threats)?;
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,260 @@
+use std::iter::{Enumerate, Peekable};
+use std::ops::RangeInclusive;
+use thiserror;
+
+use pulldown_cmark::{CodeBlockKind, Event, HeadingLevel, MetadataBlockKind, Options, Tag, TagEnd};
+use pulldown_cmark_to_cmark::cmark;
+
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("End of stream reached")]
+    EOF,
+    #[error("Expected token")]
+    Expected(String),
+}
+
+pub struct MarkdownParser<'input>{
+    events: Vec<Event<'input>>,
+}
+impl <'input> MarkdownParser<'input> {
+
+    pub fn new(markdown_input: &'input str) -> Self{
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
+        Self{
+            events: pulldown_cmark::Parser::new_ext(markdown_input, options).collect(),
+        }
+    }
+
+    pub fn to_string(&self, range: Option<RangeInclusive<usize>>) -> String{
+        range.map_or_else(|| String::new(), |r|{let mut s = String::new(); cmark((self.events[r]).iter(), &mut s).unwrap(); s},)
+    }
+
+    pub fn get_text(&self, range: RangeInclusive<usize>) -> String {
+        let mut  x = self.events[range].iter();
+        x.next(); // skip the start tag
+        let Some(Event::Text(text)) = x.next() else {
+            return "".to_string();
+        };
+        text.trim_end().to_string()
+    }
+    pub fn iter(&self) -> MarkdownIter{
+        MarkdownIter::new(&self.events)
+    }
+}
+
+
+pub struct MarkdownIter<'inner>{
+    inner: Peekable<Enumerate<std::slice::Iter<'inner, Event<'inner>>>>,
+}
+
+impl <'inner> Iterator for MarkdownIter<'inner>{
+
+    type Item = (usize, &'inner Event<'inner>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl <'inner> MarkdownIter<'inner> {
+    fn new(events: &'inner [Event<'inner>]) -> Self {
+         Self{
+             inner: events.iter().enumerate().peekable(),
+         }
+    }
+
+    fn take<'a, F>(&mut self, f: F) -> Result<(&'a Event<'inner>, usize), Error>
+    where 
+        F: Fn(&Event) -> bool
+    {
+        let i = self.next();
+        let Some((pos, e)) = i else {
+            return Err(Error::EOF);
+        };
+        if !f(e) {
+            return Err(Error::Expected(format!("{:?}", e)));
+        }
+        Ok((e, pos))
+    }
+    fn take_map<'a, 'b, F, T>(&mut self, f: F) -> Result<T, Error>
+    where 
+        F: Fn(&Event) -> Option<T>
+    {
+        let i = self.next();
+        let Some((_, e)) = i else {
+            return Err(Error::EOF);
+        };
+        match f(e) {
+            Some(value) => {
+                Ok(value)
+            }
+            None => {
+                return Err(Error::Expected(format!("{:?}", e)));
+            },
+        }
+    }
+
+    fn take_text<'a, 'b>(&mut self) -> Result<String, Error>{
+        let metadata = self.take_map(|e| match e {
+            Event::Text(text) => Some(text.to_string()),
+            _ => None
+        })?;
+        Ok(metadata)
+    }
+
+    fn check<'a, F>(&mut self, f: F) -> Result<(&'a Event<'inner>, usize), Error>
+    where 
+        F: Fn(&Event) -> bool
+    {
+        let i = self.inner.peek();
+        let Some((pos, e)) = i else {
+            return Err(Error::EOF);
+        };
+        if !f(e) {
+            return Err(Error::Expected(format!("{:?}", e)));
+        }
+        Ok((e, *pos))
+    }
+    pub fn metadata(&mut self) -> Result<String, Error> {
+        self.take(|e| e == &Event::Start(Tag::MetadataBlock(MetadataBlockKind::YamlStyle)))?;
+        let metadata = self.take_text()?;
+        self.take(|e| e == &Event::End(TagEnd::MetadataBlock(MetadataBlockKind::YamlStyle)))?;
+        Ok(metadata)
+    }
+
+    pub fn heading(&mut self, level: HeadingLevel) -> Result<String, Error> {
+        self.take(|e| {
+            let &Event::Start(Tag::Heading{level:l,..}) = e else {return false;};
+            l == level
+        })?;
+        let heading = self.take_text()?;
+        self.take(|e| {
+            let &Event::End(TagEnd::Heading(l)) = e else {return false;};
+            l == level
+        })?;
+        Ok(heading)
+    }
+
+    pub fn named_heading(&mut self, level: HeadingLevel, content: &'static str) -> Result<(), Error> {
+        if self.heading(level)? != content {
+            return Err(Error::Expected(format!("A heading with the content: {}", content)));
+        }
+        Ok(())
+    }
+
+    pub fn paragraph(&mut self) -> Result<RangeInclusive<usize>, Error> {
+        let (_, start) = self.check(|e| e == &Event::Start(Tag::Paragraph))?;
+        let Some((end, _)) = self.take_while(|(_i, x)| **x != Event::End(TagEnd::Paragraph)).last() else {
+            return Err(Error::Expected("expected a end of a Paragraph".to_string()));
+        };
+        
+        Ok(start..=end)
+    }
+
+
+    pub fn alt<Fun>(&mut self, funs: &[Fun]) -> Result<RangeInclusive<usize>, Error>
+    where 
+        Fun: Fn(&mut Self) -> Result<RangeInclusive<usize>, Error>
+    {
+        for f in funs {
+            if let Ok(a) = f(self){
+                return Ok(a);
+            }
+        }
+        Err(Error::Expected("Expected either one of the functions to return".to_string()))
+    }
+
+    pub fn multi<Fun>(&mut self, f: Fun) -> Option<RangeInclusive<usize>>
+    where 
+        Fun: Fn(&mut Self) -> Result<RangeInclusive<usize>, Error>
+    {
+        let content = self.paragraph().ok()?;
+        let start = *(content.start());
+        let mut end = *(content.end());
+
+        while let Ok(next) = f(self){
+            end = *next.end();
+        }
+        Some(start..=end)
+    }
+
+    fn code_block_fn<F>(&mut self, f: F) -> Result<RangeInclusive<usize>, Error>
+    where 
+        F: Fn(&Event) -> bool
+    {
+        let (_, start) = self.check(f)?;
+        let Some((end, _)) = self.take_while(|(_i, x)| **x != Event::End(TagEnd::CodeBlock)).last() else {
+            return Err(Error::Expected("expected a end of a CodeBlock".to_string()));
+        };
+        
+        Ok(start..=end)
+
+    }
+    pub fn code_block(&mut self) -> Result<RangeInclusive<usize>, Error> {
+        self.code_block_fn(|e|{
+            if let Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(ref _code_lang))) = e {
+                true
+            } else {
+                false
+            }
+            })
+    }
+    pub fn lang_block(&mut self, lang: &str) -> Result<RangeInclusive<usize>, Error> {
+        self.code_block_fn(|e|{
+            if let Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(ref code_lang))) = e {
+                *lang == **code_lang
+            } else {
+                false
+            }
+            })
+    }
+    fn item(&mut self) -> Result<Option<String>, Error> {
+        match self.inner.peek() {
+            Some((_i, Event::Start(Tag::Item))) => {
+                self.inner.next();
+            },
+            _ => {
+                return Ok(None);
+            }
+        }
+        let item = match self.next() {
+            Some((_i, Event::Text(s))) => {
+                //Trim empty line from output
+                s.to_string()
+            },
+            _ => {
+                return Err(Error::Expected("expected a Text block".to_string()));
+            }
+        };
+        let Some((_i, &Event::End(TagEnd::Item))) = self.next() else {
+            return Err(Error::Expected("expected a code block".to_string()));
+        };
+        Ok(Some(item))
+    }
+
+    pub fn item_list(&mut self) -> Result<Vec<String>, Error> {
+        match self.next() {
+            Some((_i, Event::Start(Tag::List(None)))) => {
+            },
+            _ => {
+                return Err(Error::Expected("expected a list block".to_string()));
+            }
+        }
+
+        let mut items = vec![];
+
+        while let Some(next) = self.item()? {
+            items.push(next);
+        }
+        let Some((_i, &Event::End(TagEnd::List(_b)))) = self.next() else {
+            return Err(Error::Expected("expected a end of list".to_string()));
+        };
+
+        Ok(items)
+    }
+    pub fn text(&mut self)-> Result<RangeInclusive<usize>, Error> {
+        self.alt(&vec![MarkdownIter::paragraph, MarkdownIter::code_block])
+    }
+}
